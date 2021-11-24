1f73682ba80a1ae56f024f15adbd4277ac027b2b
Гулидов Илья Дмитриевич <pr-20.107k-gi@NAVIATK.RU>
2021-11-24 17:28:26 +0700

    first commit

--- app/src/main/java/com/example/practic27/MainActivity.java
+++ app/src/main/java/com/example/practic27/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.practic27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
     }
-}
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.button2:
+                intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button3:
+                intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
+    }
+
--- app/src/main/java/com/example/practic27/MainActivity2.java
+++ app/src/main/java/com/example/practic27/MainActivity2.java
@@ -2,13 +2,30 @@ package com.example.practic27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.button4:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
